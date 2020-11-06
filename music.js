d0b8d4a1d15f71bf70aea9bebad895f1029737cb
James Gould <nobleburgundy@gmail.com>
2020-11-06 09:34:32 -0600

    Minor style update and removed console.logs.

--- music.js
+++ music.js
@@ -13,9 +13,7 @@ $(document).ready(function () {
       url: `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${searchWord}&api_key=${LASTFM_API_KEY}&format=json`,
       method: "POST",
     }).then(function (response) {
-      console.log(response);
       let trackArray = response.tracks.track;
-      console.log(trackArray);
       for (let i = 0; i < trackArray.length; i++) {
         let li = $("<li>");
         li.text(`${trackArray[i].name}, ${trackArray[i].artist.name}`);
