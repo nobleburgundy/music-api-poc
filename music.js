45d6a572efef683d92a71951e5ce6929759ae8bf
James Gould <nobleburgundy@gmail.com>
2020-11-06 09:08:31 -0600

    Working POC for using last.fm api.

---
+++ music.js
@@ -0,0 +1,26 @@
+const LASTFM_API_KEY = "174cadade97542fff7b88f7fc3b6a9ee";
+const LASTFM_SHARED_SECRET = "d0a5f066262e0666df0adf9c1af172da";
+let searchWord = "";
+let apiURL = `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${searchWord}&api_key=${LASTFM_API_KEY}&format=json`;
+
+$(document).ready(function () {
+  $("#music-word-form").on("submit", function (event) {
+    event.preventDefault();
+    searchWord = $("#weather-word-input").val();
+    $("#music-list").empty();
+
+    $.ajax({
+      url: `http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=${searchWord}&api_key=${LASTFM_API_KEY}&format=json`,
+      method: "POST",
+    }).then(function (response) {
+      console.log(response);
+      let trackArray = response.tracks.track;
+      console.log(trackArray);
+      for (let i = 0; i < trackArray.length; i++) {
+        let li = $("<li>");
+        li.text(`${trackArray[i].name}, ${trackArray[i].artist.name}`);
+        $("#music-list").append(li);
+      }
+    });
+  });
+});
